0df247570b98f040a9f6811434ce5e5394b3656c
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-12-16 04:08:36 +0200

    Update index.js

--- Script/index.js
+++ Script/index.js
@@ -1,29 +1,28 @@
-function loadAndParseNews(fiidi) {
-    let xmlhttp = new XMLHttpRequest();
+function loadAndParseNews() {
+    let feedUrl = "https://cors-anywhere.herokuapp.com/http://www.iltalehti.fi/rss/uutiset.xml";
+    var xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.open("GET", fiidi, true);
+    xmlhttp.open("GET", feedUrl, true);
     xmlhttp.send();
 
     xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
             let xmldoc = xmlhttp.responseXML;
-
             let items = xmldoc.getElementsByTagName("item");
 
             let output = "<ul>";
-
             for (let i = 0; i < items.length; i++) {
                 let title = items[i].getElementsByTagName("title")[0].textContent;
                 let link = items[i].getElementsByTagName("link")[0].textContent;
 
                 output += `<li><a href="${link}" target="_blank">${title}</a></li>`;
             }
-
             output += "</ul>";
 
             document.getElementById("newsfeed").innerHTML = output;
-        } else if (xmlhttp.readyState == 4) {
-            document.getElementById("newsfeed").innerHTML = "Ei toimi! Pääsy hylätty linkkeihin, mutta koodini löytyy!";
+        } else if (xmlhttp.readyState === 4) {
+            document.getElementById("newsfeed").innerHTML =
+                "<p>Failed to load the newsfeed. Please try again later.</p>";
         }
     };
 }
